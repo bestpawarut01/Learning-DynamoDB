fb8b27e5cdc53ac1fed45f8c6af32911d450d5cb
Pawarut_PN <bestkam.789@gmail.com>
2023-09-18 17:08:46 +0700

    New upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+require("dotenv").config();
+// require("./database/mongoose");
+
+const UserRouter = require("./src/routers/users");
+const PetRouter = require("./src/routers/users");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(UserRouter);
+app.use(PetRouter);
+
+app.listen(port, () => {
+	console.log("Server is up on port " + port);
+});

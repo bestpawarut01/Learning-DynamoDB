cb00010e2ce864714f85f34c6804c208588ec07d
Pawarut_PN <bestkam.789@gmail.com>
2023-09-19 15:27:50 +0700

    update pet

--- src/control/petControl.js
+++ src/control/petControl.js
@@ -3,9 +3,9 @@ require("dotenv");
 require("aws-sdk/lib/maintenance_mode_message").suppress = true;
 
 AWS.config.update({
-	region: process.env.AWS_DEFAULT_REGION,
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_DEFAULT_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
@@ -16,136 +16,135 @@ const Pet = require("../models/Pet_Collection");
 const User = require("../models/User_Collection");
 
 exports.AddPet = async (req, res) => {
-	try {
-		const params = {
-			TableName: PET_TABLE,
-		};
+  try {
+    const params = {
+      TableName: PET_TABLE,
+    };
 
-		// scan ข้อมูลของ pet ออกมาเก็บ
-		const allPet = await dynamoClient.scan(params).promise();
+    // scan ข้อมูลของ pet ออกมาเก็บ
+    const allPet = await dynamoClient.scan(params).promise();
 
-		let noIDpet = 201;
-		if (allPet.Items.length > 0) {
-			// map id แต่ละตัวออกมาเก็บใน idPet รูปแบบ Array
-			const idPet = allPet.Items.map((item) => parseInt(item.id, 10));
+    let noIDpet = 201;
+    if (allPet.Items.length > 0) {
+      // map id แต่ละตัวออกมาเก็บใน idPet รูปแบบ Array
+      const idPet = allPet.Items.map((item) => parseInt(item.id, 10));
 
-			noIDpet = Math.max(...idPet) + 1; // นำข้อมูลใน Array มาหาค่ามากที่สุดและ +1
-		}
+      noIDpet = Math.max(...idPet) + 1; // นำข้อมูลใน Array มาหาค่ามากที่สุดและ +1
+    }
 
-		const tempData = {
-			// สร้างเพื่อเก็บข้อมูลรวมไว้ก่อน ก่อนที่จะนำเข้า Query
-			id: noIDpet.toString(),
-			petName: req.body.petName,
-			petType: req.body.petType,
-		};
+    const tempData = {
+      // สร้างเพื่อเก็บข้อมูลรวมไว้ก่อน ก่อนที่จะนำเข้า Query
+      id: noIDpet.toString(),
+      petName: req.body.petName,
+      petType: req.body.petType,
+    };
 
-		const query = {
-			// ใน query มีชื่อ table , ข้อมูลรวมไว้ก่อนหน้า
-			TableName: PET_TABLE,
-			Item: tempData,
-		};
+    const query = {
+      // ใน query มีชื่อ table , ข้อมูลรวมไว้ก่อนหน้า
+      TableName: PET_TABLE,
+      Item: tempData,
+    };
 
-		await dynamoClient.put(query).promise();
+    await dynamoClient.put(query).promise();
 
-		res.status(200).json({ query });
-	} catch (err) {
-		res.status(400).send(err);
-	}
+    res.status(200).json({ query });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 
 exports.DeletePet = async (req, res) => {
-	try {
-		const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-		const params = {
-			// params คือการ scan ว่ามีข้อมูลเป็นหรือไม่
-			TableName: PET_TABLE,
-			FilterExpression: "id = :idValue",
-			ExpressionAttributeValues: {
-				":idValue": req.params.id,
-			},
-		};
+    const params = {
+      // params คือการ scan ว่ามีข้อมูลเป็นหรือไม่
+      TableName: PET_TABLE,
+      FilterExpression: "id = :idValue",
+      ExpressionAttributeValues: {
+        ":idValue": req.params.id,
+      },
+    };
 
-		const allPet = await dynamoClient.scan(params).promise();
+    const allPet = await dynamoClient.scan(params).promise();
 
-		if (allPet.Items.length > 0) {
-			// ถ้ามีจะทำการลบ
-			const tempDel = {
-				TableName: PET_TABLE,
-				Key: { id },
-			};
+    if (allPet.Items.length > 0) {
+      // ถ้ามีจะทำการลบ
+      const tempDel = {
+        TableName: PET_TABLE,
+        Key: { id },
+      };
 
-			await dynamoClient.delete(tempDel).promise();
-			console.log(tempDel);
+      await dynamoClient.delete(tempDel).promise();
+      console.log(tempDel);
 
-			res.status(200).send("Delet pet successfully.");
-		} else {
-			// ถ้าไม่มีจะแจ้งเตือน
-			res.status(404).send("Not Found pet.!");
-		}
-	} catch (error) {
-		res.status(400).send(error);
-	}
+      res.status(200).send("Delet pet successfully.");
+    } else {
+      // ถ้าไม่มีจะแจ้งเตือน
+      res.status(404).send("Not Found pet.!");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
 exports.FindPet = async (req, res) => {
-	try {
-		const params = {
-			// params คือการ scan ว่ามีข้อมูลเป็นหรือไม่
-			TableName: PET_TABLE,
-			FilterExpression: "id = :idValue",
-			ExpressionAttributeValues: {
-				":idValue": req.params.id,
-			},
-		};
+  try {
+    const params = {
+      // params คือการ scan ว่ามีข้อมูลเป็นหรือไม่
+      TableName: PET_TABLE,
+      FilterExpression: "id = :idValue",
+      ExpressionAttributeValues: {
+        ":idValue": req.params.id,
+      },
+    };
 
-		const allPet = await dynamoClient.scan(params).promise();
+    const allPet = await dynamoClient.scan(params).promise();
 
-		if (allPet.Items.length > 0) {
-			res.status(200).send(allPet.Items);
-		} else {
-			res.status(404).send("Not Found pet.!");
-		}
-	} catch (error) {
-		res.status(500).send();
-	}
+    if (allPet.Items.length > 0) {
+      res.status(200).send(allPet.Items);
+    } else {
+      res.status(404).send("Not Found pet.!");
+    }
+  } catch (error) {
+    res.status(500).send();
+  }
 };
 
 exports.UpdatePet = async (req, res) => {
-	try {
-		const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-		const params = {
-			// params คือการ scan ว่ามีข้อมูลเป็นหรือไม่
-			TableName: PET_TABLE,
-			FilterExpression: "id = :idValue",
-			ExpressionAttributeValues: {
-				":idValue": req.params.id,
-			},
-		};
+    const params = {
+      // params คือการ scan ว่ามีข้อมูลเป็นหรือไม่
+      TableName: PET_TABLE,
+      FilterExpression: "id = :idValue",
+      ExpressionAttributeValues: {
+        ":idValue": req.params.id,
+      },
+    };
 
-		const allPet = await dynamoClient.scan(params).promise();
+    const allPet = await dynamoClient.scan(params).promise();
 
-		if (allPet.Items.length > 0) {
-			const tempUpdate = {
-				id: id,
-				petName: req.body.petName,
-				petType: req.body.petType,
-			};
+    if (allPet.Items.length > 0) {
+      const tempUpdate = {
+        id: id,
+        petName: req.body.petName,
+        petType: req.body.petType,
+      };
 
-			const queryUpdate = {
-				TableName: PET_TABLE,
-				Item: tempUpdate,
-			};
+      const queryUpdate = {
+        TableName: PET_TABLE,
+        Item: tempUpdate,
+      };
 
-			await dynamoClient.put(queryUpdate).promise();
+      await dynamoClient.put(queryUpdate).promise();
 
-			res.status(200).json({ queryUpdate });
-		} else {
-			res.status(404).send("Not Found pet.!");
-		}
-		return res.status(200).send();
-	} catch (e) {
-		res.status(400).send(e);
-	}
+      res.status(200).json({ queryUpdate });
+    } else {
+      res.status(404).send("Not Found pet.!");
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
 };
--- src/control/userControl.js
+++ src/control/userControl.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 require("aws-sdk/lib/maintenance_mode_message").suppress = true;
 
 AWS.config.update({
-	region: process.env.AWS_DEFAULT_REGION,
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_DEFAULT_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
@@ -13,180 +13,171 @@ const USER_TABLE = process.env.USER_TABLE;
 const PET_TABLE = process.env.PET_TABLE;
 
 exports.AddUser = async (req, res) => {
-	try {
-		const user = req.body;
-		const params = {
-			TableName: USER_TABLE,
-		};
+  try {
+    const user = req.body;
+    const params = {
+      TableName: USER_TABLE,
+    };
 
-		// 1. สแกน DynamoDB เพื่อหาข้อมูลที่มีอยู่ในตาราง user
-		const existingUser = await dynamoClient.scan(params).promise();
+    // 1. สแกน DynamoDB เพื่อหาข้อมูลที่มีอยู่ในตาราง user
+    const existingUser = await dynamoClient.scan(params).promise();
 
-		// 2. หา id ใหม่สำหรับ user
-		let newId = 101; // ค่าเริ่มต้นหากไม่มีข้อมูลในตาราง
+    // 2. หา id ใหม่สำหรับ user
+    let newId = 101; // ค่าเริ่มต้นหากไม่มีข้อมูลในตาราง
 
-		if (existingUser.Items.length > 0) {
-			const idInUse = existingUser.Items.map((item) =>
-				parseInt(item.id, 10)
-			);
-			newId = Math.max(...idInUse) + 1;
-		}
+    if (existingUser.Items.length > 0) {
+      const idInUse = existingUser.Items.map((item) => parseInt(item.id, 10));
+      newId = Math.max(...idInUse) + 1;
+    }
 
-		// 3. ตรวจสอบว่า username ซ้ำหรือไม่
-		const usernameInUse = existingUser.Items.some(
-			(item) =>
-				item.UserName.toLowerCase() === user.UserName.toLowerCase()
-		);
+    // 3. ตรวจสอบว่า username ซ้ำหรือไม่
+    const usernameInUse = existingUser.Items.some(
+      (item) => item.UserName.toLowerCase() === user.UserName.toLowerCase()
+    );
 
-		if (usernameInUse) {
-			return res
-				.status(400)
-				.json({ message: "Username already in use." });
-		}
+    if (usernameInUse) {
+      return res.status(400).json({ message: "Username already in use." });
+    }
 
-		// 4. กำหนด id ใหม่และเตรียมข้อมูล pet
-		user.id = newId.toString();
+    // 4. กำหนด id ใหม่และเตรียมข้อมูล pet
+    user.id = newId.toString();
 
-		// 5. สร้าง id ของ pet
-		const paramsPET = {
-			TableName: PET_TABLE,
-		};
-		const reportPet = await dynamoClient.scan(paramsPET).promise();
+    // 5. สร้าง id ของ pet
+    const paramsPET = {
+      TableName: PET_TABLE,
+    };
+    const reportPet = await dynamoClient.scan(paramsPET).promise();
 
-		let newIdPet = 201;
+    let newIdPet = 201;
 
-		if (reportPet.Items.length > 0) {
-			const idInPet = reportPet.Items.map((item) =>
-				parseInt(item.id, 10)
-			);
-			newIdPet = Math.max(...idInPet) + 1;
-		}
+    if (reportPet.Items.length > 0) {
+      const idInPet = reportPet.Items.map((item) => parseInt(item.id, 10));
+      newIdPet = Math.max(...idInPet) + 1;
+    }
 
-		// 6. สร้างข้อมูลสัตว์เลี้ยง
-		const petList = {
-			id: newIdPet.toString(),
-			petName: user.petName,
-		};
+    // 6. สร้างข้อมูลสัตว์เลี้ยง
+    const petList = {
+      id: newIdPet.toString(),
+      petName: user.petName,
+    };
 
-		// สร้าง Araray ให้กับรายชื่อ petName เพื่อรอรับการเก็บแบบ Array
-		const tempArr = [];
-		tempArr.push(newIdPet.toString());
+    // สร้าง Araray ให้กับรายชื่อ petName เพื่อรอรับการเก็บแบบ Array
+    const tempArr = [];
+    tempArr.push(newIdPet.toString());
 
-		req.body.petName = tempArr;
+    req.body.petName = tempArr;
 
-		// 7. สร้าง query สำหรับ user
-		const query = {
-			TableName: USER_TABLE,
-			Item: user,
-		};
+    // 7. สร้าง query สำหรับ user
+    const query = {
+      TableName: USER_TABLE,
+      Item: user,
+    };
 
-		// 8. สร้าง query สำหรับ pet
-		const queryPet = {
-			TableName: PET_TABLE,
-			Item: petList,
-		};
+    // 8. สร้าง query สำหรับ pet
+    const queryPet = {
+      TableName: PET_TABLE,
+      Item: petList,
+    };
 
-		// 9. อัปโหลดข้อมูลไปยัง DynamoDB ด้วย query ของ user และ pet
-		await Promise.all([
-			dynamoClient.put(query).promise(),
-			dynamoClient.put(queryPet).promise(),
-		]);
+    // 9. อัปโหลดข้อมูลไปยัง DynamoDB ด้วย query ของ user และ pet
+    await Promise.all([
+      dynamoClient.put(query).promise(),
+      dynamoClient.put(queryPet).promise(),
+    ]);
 
-		res.status(200).send("Input successfully");
-	} catch (err) {
-		res.status(400).json(err);
-	}
+    res.status(200).send("Input successfully");
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
 
 exports.DeleteUser = async (req, res) => {
-	try {
-		const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-		const query = {
-			TableName: USER_TABLE,
-			Key: { id },
-		};
+    const query = {
+      TableName: USER_TABLE,
+      Key: { id },
+    };
 
-		console.log(query);
-		await dynamoClient.delete(query).promise();
+    console.log(query);
+    await dynamoClient.delete(query).promise();
 
-		res.status(200).send("Delete Success");
-	} catch (err) {
-		res.status(400).json(err);
-	}
+    res.status(200).send("Delete Success");
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
 
 exports.FindUser = async (req, res) => {
-	try {
-		const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-		const query = {
-			TableName: USER_TABLE,
-			Key: { id },
-		};
+    const query = {
+      TableName: USER_TABLE,
+      Key: { id },
+    };
 
-		const findUser = await dynamoClient.get(query).promise();
+    const findUser = await dynamoClient.get(query).promise();
 
-		res.status(201).send(findUser);
-	} catch (error) {
-		res.status(500).send();
-	}
+    res.status(201).send(findUser);
+  } catch (error) {
+    res.status(500).send();
+  }
 };
 
 exports.UpdateUser = async (req, res) => {
-	try {
-		const { id } = req.params;
-		const params = {
-			TableName: USER_TABLE,
-			Key: { id },
-		};
+  try {
+    const { id } = req.params;
+    const params = {
+      TableName: USER_TABLE,
+      Key: { id },
+    };
 
-		// 1. ค้นหาผู้ใช้ด้วย id
-		const findUser = await dynamoClient.get(params).promise();
+    // 1. ค้นหาผู้ใช้ด้วย id
+    const findUser = await dynamoClient.get(params).promise();
 
-		// ถ้าไม่พบผู้ใช้หรือ object ว่าง
-		if (!findUser.Item) {
-			return res
-				.status(400)
-				.json({ message: "Undefined id or username." });
-		}
+    // ถ้าไม่พบผู้ใช้หรือ object ว่าง
+    if (!findUser.Item) {
+      return res.status(400).json({ message: "Undefined id or username." });
+    }
 
-		// 2. สร้างข้อมูล temp สำหรับเก็บลงใน Table User
-		const tempData = {
-			id: id,
-			firstName: req.body.firstName,
-			lastName: req.body.lastName,
-			age: req.body.age,
-			UserName: findUser.Item.UserName,
-			petName: findUser.Item.petName,
-		};
+    // 2. สร้างข้อมูล temp สำหรับเก็บลงใน Table User
+    const tempData = {
+      id: id,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      age: req.body.age,
+      UserName: findUser.Item.UserName,
+      petName: findUser.Item.petName,
+    };
 
-		// 3. สร้าง query สำหรับเก็บลงใน Table User
-		const query = {
-			TableName: USER_TABLE,
-			Item: tempData,
-		};
+    // 3. สร้าง query สำหรับเก็บลงใน Table User
+    const query = {
+      TableName: USER_TABLE,
+      Item: tempData,
+    };
 
-		// 4. สร้างข้อมูล temp สำหรับเก็บลงใน Table Pet
-		const petList = {
-			id: findUser.Item.petName[0],
-			petName: req.body.petName,
-		};
+    // 4. สร้างข้อมูล temp สำหรับเก็บลงใน Table Pet
+    const petList = {
+      id: findUser.Item.petName[0],
+      petName: req.body.petName,
+    };
 
-		// 5. สร้าง query สำหรับเก็บลงใน Table Pet
-		const queryPet = {
-			TableName: PET_TABLE,
-			Item: petList,
-		};
+    // 5. สร้าง query สำหรับเก็บลงใน Table Pet
+    const queryPet = {
+      TableName: PET_TABLE,
+      Item: petList,
+    };
 
-		// 6. อัปโหลดข้อมูลไปยัง DynamoDB ด้วย query ของ User และ Pet
-		await Promise.all([
-			dynamoClient.put(query).promise(),
-			dynamoClient.put(queryPet).promise(),
-		]);
+    // 6. อัปโหลดข้อมูลไปยัง DynamoDB ด้วย query ของ User และ Pet
+    await Promise.all([
+      dynamoClient.put(query).promise(),
+      dynamoClient.put(queryPet).promise(),
+    ]);
 
-		res.status(200).json(tempData);
-	} catch (e) {
-		res.status(400).send(e);
-	}
+    res.status(200).json(tempData);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 };
